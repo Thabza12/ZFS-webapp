2b8b6b1b4635587a4e7dfc0a3c4ac9b8aa507dd2
Thabza12 <sanelet.cele@gmail.com>
2022-01-10 11:13:20 +0200

    initial main branch commit

---
+++ ZFS (1)/ZFS/src/main/java/com/vico/ZFS/ZfsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vico.ZFS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZfsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZfsBackendApplication.class, args);
+	}
+
+}
---
+++ ZFS (1)/ZFS/src/test/java/com/vico/ZFS/ZfsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vico.ZFS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZfsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
